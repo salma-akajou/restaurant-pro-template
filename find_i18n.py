292cd775424008f46992c75f6a8bf27540497ee3
Yousra-akg <akagouyoussra@gmail.com>
2026-06-26 11:04:33 +0100

    remove: temporary python scripts

--- find_i18n.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open(r'c:\Users\Gebruiker\Desktop\restaurant-pro-template\maquettes\about.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-matches = re.findall(r'data-i18n=["\']([^"\']+)["\']', html)
-print(set(matches))
